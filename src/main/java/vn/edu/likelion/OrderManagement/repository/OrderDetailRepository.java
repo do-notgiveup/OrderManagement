6fb0ec9582faa539a136690677976461cec9832a
Rains97 <hvinhtuong@gmail.com>
2024-08-23 15:11:17 +0700

    Update OrderController

--- src/main/java/vn/edu/likelion/OrderManagement/repository/OrderDetailRepository.java
+++ src/main/java/vn/edu/likelion/OrderManagement/repository/OrderDetailRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vn.edu.likelion.OrderManagement.entity.OrderDetailEntity;
+import vn.edu.likelion.OrderManagement.model.OrderDetailRequest;
 
 import java.time.LocalDate;
 import java.util.List;
